93a6b4b053d69cb195786959614054ff399d7874
Kadet <techkadet@gmail.com>
2020-09-01 19:57:29 +0100

    added menu toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,8 @@ document.querySelector('#home header .menu').addEventListener('click', () => {
   document.querySelector('#home header .mobile_menu').classList.toggle('show');
 })
 
+document.querySelectorAll('#home header .mobile_menu ul a').forEach(menu => {
+  menu.addEventListener('click', () => {
+    document.querySelector('#home header .mobile_menu').classList.toggle('show');
+  })
+})
